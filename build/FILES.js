90a090d9f0ab0fa86dd9e3e89e60578a07654473
yunzhao.li <516911028@qq.com>
2018-02-27 20:07:07 +0800

    fix

--- build/FILES.js
+++ build/FILES.js
@@ -5,18 +5,18 @@ module.exports = [
   },
   {
     filename: 'NormalPopup',
-    title: '通用弹窗',
+    title: '<a href="https://dom-bro.github.io/views/%E6%95%88%E6%9E%9C/effect_04_%E9%80%9A%E7%94%A8%E5%BC%B9%E7%AA%97/index">通用弹窗</a>',
   },
   {
     filename: 'SlidePopup',
-    title: '底部浮现弹窗',
+    title: '<a href="https://dom-bro.github.io/views/%E6%95%88%E6%9E%9C/effect_02_%E5%BA%95%E9%83%A8%E6%B5%AE%E7%8E%B0%E5%BC%B9%E7%AA%97/index">底部浮现弹窗</a>',
   },
   {
     filename: 'Floor',
-    title: '楼层定位',
+    title: '<a href="https://dom-bro.github.io/views/%E6%95%88%E6%9E%9C/effect_00_%E6%A5%BC%E5%B1%82%E5%AE%9A%E4%BD%8D%E6%95%88%E6%9E%9C/index">楼层定位</a>',
   },
   {
     filename: 'CountDown',
-    title: '倒计时',
+    title: '<a href="https://dom-bro.github.io/views/%E5%A4%8D%E7%94%A8/CountDown_%E5%80%92%E8%AE%A1%E6%97%B6/index">倒计时</a>',
   },
 ]
